4d3ce6884c376b0b955c1e5521f43d9562f6ccfc
BrianAmin <tuf49787@temple.edu>
2017-04-10 10:24:24 -0400

    moar code

--- teleop-imgrequestrec.py
+++ teleop-imgrequestrec.py
@@ -2,5 +2,43 @@
 
 import pygame as pyg
 import cv2
-import socket
+from socket import *
 
+#target Main base computer information
+targetIP = '192.168.1.100'
+
+port = 15000
+serverAddress = (targetIP, port)
+
+#recieving port on Nuc
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+serverSocket.bind(('', port))
+
+
+
+
+
+def sendimg():
+    print("sending image")
+    #Init instance of cameras for teleop visual feedback
+    cam1 = cv2.VideoCapture(0)
+    cam2 = cv2.VideoCapture(1)
+    ret1, cam1_img = cam1.read()
+    ret2, cam2_img = cam2.read()
+
+
+while True:
+    message, address = serverSocket.recvfrom(1024)
+
+    if message = "img":
+        sendimg()
+
+
+
+        at frame;
+frame = (frame.reshape(0,1)); // to make it continuous
+
+int  imgSize = frame.total()*frame.elemSize();
+
+// Send data here
+bytes = send(clientSock, frame.data, imgSize, 0))
